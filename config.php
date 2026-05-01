b3a211ddcc91435825b4ad5499a659583727d4fb
Juan Antonio Vera López <javeralopez@gmail.com>
2026-05-01 12:44:25 -0600

    Mejorar seguridad: config.php con variables de entorno y validación

--- config.php
+++ config.php
@@ -1,79 +1,95 @@
 <?php
 /*
    (c) iRadeo.com
+   Configuración mejorada con soporte para variables de entorno
 */
 
+// Cargar variables de entorno (si usas un archivo .env)
+if (file_exists(__DIR__ . '/.env')) {
+    $env = parse_ini_file(__DIR__ . '/.env');
+    foreach ($env as $key => $value) {
+        putenv("$key=$value");
+    }
+}
 
+/**
+ * Player Title
+ */
+$title = getenv('PLAYER_TITLE') ?: 'Radionoticias';
 
-
-/*
-   Your Player Title
-*/
-
-$title = 'Radionoticias';
-
-/*
-   Label Display Feature
-   Display labels inside player.
-   Set any label to '' to hide it.
-*/
-
-    $labels = array(
-    'song' => 'Cancion/Programa',
+/**
+ * Label Display Feature
+ * Display labels inside player.
+ * Set any label to '' to hide it.
+ */
+$labels = array(
+    'song'   => 'Cancion/Programa',
     'artist' => 'Autor',
-    'album' => 'Album'
-
+    'album'  => 'Album'
 );
 
-/*
-   Root directory to your mp3 folder.(eg: /root/mp3s/)
-   Directory where your audio files are stored.
-   If you need assistance, request this information from your web hosting provider.
-*/
+/**
+ * Root directory to your mp3 folder
+ * Use absolute paths for better security
+ */
+$mp3_dir = getenv('MP3_DIR') ?: '/home/estudian/public_html/radio/mp3s/';
 
-$mp3_dir = '/home/estudian/public_html/radio/mp3s/';
+// Validar que el directorio existe
+if (!is_dir($mp3_dir)) {
+    die('Error: El directorio de MP3s no existe: ' . htmlspecialchars($mp3_dir));
+}
 
-/*
-   Public address for your audio folder.
-   If you need assistance, request this information from your web hosting provider.
-*/
+// Asegurar que termina con /
+if (substr($mp3_dir, -1) !== '/') {
+    $mp3_dir .= '/';
+}
 
-$http_path = 'http://estudiantesdecomunicacion.com/radio/mp3s/';
+/**
+ * Public address for your audio folder
+ */
+$http_path = getenv('HTTP_PATH') ?: 'http://estudiantesdecomunicacion.com/radio/mp3s/';
 
-/*
-   Shuffle Mode Feature
-   Enabled - Streams files randomly from specified directory.
-   Disabled - Sorts files alphabetically by filename/pathname and play sequentially.
-   Enter true to enable or false to disable feature.
-*/
+// Asegurar que termina con /
+if (substr($http_path, -1) !== '/') {
+    $http_path .= '/';
+}
 
-$shuffle = false;
+/**
+ * Shuffle Mode Feature
+ * Enabled - Streams files randomly from specified directory.
+ * Disabled - Sorts files alphabetically by filename/pathname and play sequentially.
+ * Enter true to enable or false to disable feature.
+ */
+$shuffle = (getenv('SHUFFLE_MODE') === 'true') ? true : false;
 
-/*
-   Skip Feature
-   Limits the number of skips before having to stream one whole audio file.
-   Unlimited skips: -1
-   No skipping: 0
-   X Skips (then must listen to entire audio): 1+
-*/
+/**
+ * Skip Feature
+ * Limits the number of skips before having to stream one whole audio file.
+ * Unlimited skips: -1
+ * No skipping: 0
+ * X Skips (then must listen to entire audio): 1+
+ */
+$skip_limit = (int) (getenv('SKIP_LIMIT') ?: 5);
 
-$skip_limit = 5;
-
-/*
-   File Type Supported
-   DO NOT EDIT
-   Only .wav and .mp3 will work.
-*/
-
+/**
+ * File Type Supported
+ * Only .wav and .mp3 will work.
+ */
 $playable = array('mp3', 'wav');
 
-/*
-   Auto Play Feature
-   Enabled - Streams files automatically when web page loads.
-   Disabled - Requires users to click on play button to start streaming.
-   Enter true to enable or false to disable feature.
-*/
+/**
+ * Auto Play Feature
+ * Enabled - Streams files automatically when web page loads.
+ * Disabled - Requires users to click on play button to start streaming.
+ * Enter true to enable or false to disable feature.
+ */
+$auto_play = (getenv('AUTO_PLAY') === 'true') ? true : false;
 
-$auto_play = true;
+/**
+ * Configuración de seguridad
+ */
+define('SESSION_TIMEOUT', getenv('SESSION_TIMEOUT') ?: 3600); // 1 hora
+define('MAX_FILE_SIZE', getenv('MAX_FILE_SIZE') ?: 104857600); // 100MB
+define('DEBUG_MODE', getenv('DEBUG_MODE') === 'true' ? true : false);
 
 ?>
